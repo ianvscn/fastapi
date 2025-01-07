3ebaa23edb9f196db80e7792c701cdc3b2b208a2
Ian Vasconcelos <ianvscn@gmail.com>
2025-01-07 17:55:39 -0300

    Section 15
    project 4 - unit & integration testing

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,61 +1,10 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
-from ..database import Base
-from ..main import app
 from ..routers.todos import get_db, get_current_user
-from fastapi.testclient import TestClient
 from fastapi import status
-import pytest
 from ..models import Todos
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={'check_same_thread': False},
-    poolclass=StaticPool,
-)
-
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-def override_get_current_user():
-    return {'username': 'chico', 'id': 1, 'user_role': 'admin'}
+from .utils import *
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
-
-client = TestClient(app)
-
-
-@pytest.fixture
-def test_todo():
-    todo = Todos(
-        title="Learn to code",
-        description="Need to learn everyday!",
-        priority=5,
-        complete=False,
-        owner_id=1,
-    )
-
-    db = TestingSessionLocal()
-    db.add(todo)
-    db.commit()
-    yield todo
-    with engine.connect() as connection:
-        connection.execute(text("DELETE FROM todos;"))
-        connection.commit()
 
 
 def test_read_all_authenticated(test_todo):
@@ -112,21 +61,29 @@ def test_update_todo(test_todo):
     model = db.query(Todos).filter(Todos.id == 1).first()
     assert model.title == 'Change the title of the todo already saved!'
 
+def test_update_todo_not_found(test_todo):
+    request_data={
+        'title': 'Change the title of the todo already saved!',
+        'description': 'Need to learn every day!',
+        'priority': 5,
+        'complete': False,
+    }
+
+    response = client.put('/todo/999', json=request_data)
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found'}
 
 
+def test_delete_todo(test_todo):
+    response = client.delete('/todo/1')
+    assert response.status_code == 204
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+def test_delete_todo_not_found(test_todo):
+    response = client.delete('/todo/999')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found'}
 
