a61799f3108024be8f9ff8cc97761d8e244db5bb
Ian Vasconcelos <ianvscn@gmail.com>
2025-01-10 12:35:10 -0300

    ajustando para render

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,8 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://username:password@hostname:port/database_name"
+# SQLALCHEMY_DATABASE_URL = "postgresql://username:password@hostname:port/database_name"
+
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
+#caso altere para banco local
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
